457de580a7a5602d6c95ab20269cde58fa6516f3
falskuyyy <ahmdfalah10@gmail.com>
2024-12-16 20:38:28 +0700

    Menambahkan implementasi room database dengan entitas Mahasiswa dan MahasiswaDao.

--- app/src/main/java/com/example/roomlocaldb/data/database/krsDatabase.kt
+++ app/src/main/java/com/example/roomlocaldb/data/database/krsDatabase.kt
@@ -9,11 +9,10 @@ import com.example.roomlocaldb.data.entity.Mahasiswa
 
 @Database(entities = [Mahasiswa::class], version = 1, exportSchema = false)
 abstract class krsDatabase : RoomDatabase(){
-    //Mendefinisikan fungsi untuk mengakses data Mahasiswa
     abstract fun mahasiswaDao(): MahasiswaDao
 
     companion object {
-        @Volatile // Memastikan bahwa nilai variabel Instance selalu sama di semua thread
+        @Volatile
         private var Instance: krsDatabase? = null
 
         fun getDatabase(context: Context) : krsDatabase {
