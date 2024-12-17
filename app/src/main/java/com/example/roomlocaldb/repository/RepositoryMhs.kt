d40fd957a76900782ce6c86e00684b1b9867a066
falskuyyy <ahmdfalah10@gmail.com>
2024-12-17 10:57:57 +0700

    Menambahkan interface RepositoryMhs untuk mendefinisikan operasi CRUD (Create, Read, Update, Delete) pada entitas Mahasiswa menggunakan coroutine dan Flow

--- app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
@@ -1,7 +1,15 @@
 package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+    //getMhs
+    fun getMhs(nim : String): Flow<Mahasiswa>
+    //deleteMhs
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    //updateMhs
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
